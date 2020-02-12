d217d6f811cc7c927f93f0e928354423b56f60cd
mstech66 <manthanmevada66@gmail.com>
2020-02-12 23:09:37 +0530

    Initial Commit

---
+++ src/AnonymousClass.java
@@ -0,0 +1,68 @@
+/**
+ * Anonymous Classes
+ * Classes without names as we know it and yeah just single object.
+ * How different from normal classes?
+ * No constructors
+ * No multiple interface impl or class extends
+ * No simultaneous impl or extends just one thing at a time either you use a Class or an interface
+ * Stuff to know:
+ * it can access local variables in the class which it is impl for ex. id (int)
+ * also it can access variables from interface or class
+ * Limitations:
+ * No static blocks
+ * Can use constants/variables declared within the braces of it
+ * Can use local methods inside the block but can't use it with instance as Student interface doesn't have it
+ */
+interface Student{
+    String school = "VVM";
+    void getName();
+    void getId();
+}
+
+public class AnonymousClass {
+    public static void main(String[] args) {
+        int id = 560326;
+        Student student = new Student() {
+            private static final String name = "Manoj"; //should be final to use a static variable
+            @Override
+            public void getName() {
+                System.out.println("Student " + name + " from "+ school);
+            }
+
+            @Override
+            public void getId() {
+                System.out.println("ID: " + id);
+            }
+            public void leave(){
+                System.out.println("Left!");
+            }
+        };
+        student.getName();
+        student.getId();
+        //with class extends
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                System.out.println("Run called!!");
+            }
+        };
+        thread.start();
+        //with interface
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Run of Runnable called!");
+            }
+        };
+        Thread t = new Thread(runnable);
+        t.start();
+        //within constructor (used in designing GUI)
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Run of Constructor called!");
+            }
+        });
+        thread1.start();
+    }
+}
---
+++ src/JaggedArray.java
@@ -0,0 +1,23 @@
+public class JaggedArray {
+    public static void main(String[] args) {
+        int[][] intArray = new int[4][];
+        intArray[0] = new int[1];
+        intArray[1] = new int[2];
+        intArray[2] = new int[3];
+        intArray[3] = new int[4];
+
+        int k = 0;
+        for(int i=0;i<4;i++){
+            for(int j= 0;j< i+1;j++){
+                intArray[i][j] = k;
+                k++;
+            }
+        }
+        for(int i=0;i<4;i++){
+            for(int j= 0;j< i+1;j++){
+                System.out.print(intArray[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
