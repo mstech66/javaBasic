045af4c89af1a06edceaa317d6fc30ff1f43cc26
mstech66 <manthanmevada66@gmail.com>
2020-02-24 21:57:32 +0530

    Added Generics and Default Method

---
+++ src/DefaultMethodDemo.java
@@ -0,0 +1,53 @@
+/**
+ * Default Methods
+ * 
+ * An interface has generally a method declaration which has to be defined when a class implements interface.
+ * Default methods were introduced to solve this problem so that you don't have to define it in every implementation.
+ * Static methods in interface are similar to static methods or classes.
+ */
+
+interface DemoInterface{
+    public void demoMethod();
+
+    default void show(){
+        System.out.println("Default Method is running!");
+    }
+
+    static void staticShow(){
+        System.out.println("Static Show is running!");
+    }
+}
+
+interface Interface1{
+    default void show(){
+        System.out.println("Show in Interface 1!");
+    }
+}
+
+interface Interface2{
+    default void show(){
+        System.out.println("Show in Interface 2");
+    }
+}
+
+//You must override the default method when it has the same method name in two different interfaces and a class implements them.
+class InterfaceImp implements Interface1, Interface2{
+    @Override
+    public void show() {
+        Interface2.super.show();
+    }
+}
+
+public class DefaultMethodDemo implements DemoInterface{
+    public static void main(String[] args) {
+        DefaultMethodDemo demo = new DefaultMethodDemo();
+        demo.demoMethod();
+        demo.show();
+        DemoInterface.staticShow();
+    }
+
+    @Override
+    public void demoMethod() {
+        System.out.println("Demo Method in DefaultMethodDemo class");
+    }
+}
---
+++ src/GenericDemo.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericDemo {
+
+    static Integer[] intArray = {1,2,3,4,5,6,7,8};
+
+    // public static List arrayToList(Object[] array, List<Object> list){
+    //     for(Object object: array){
+    //         list.add(object);
+    //     }
+    //     return list;
+    // }
+
+    public static <T> List<T> arrayToList(T[] array, List<T> list){
+        for(T object: array){
+            list.add(object);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        // List<String> integers = arrayToList(intArray, new ArrayList<>()); //this is an error but it doesn't show in the compile time
+        // List<String> integers = arrayToList(intArray, new ArrayList<>()); this will show you the error in compile-time
+        List<Integer> integers = arrayToList(intArray, new ArrayList<>());
+        System.out.println(integers.get(2));
+        displayStrings("This", "is", "Manthan", "Mevada");
+    }
+
+    public static void displayStrings(String... strings){
+        StringBuilder sb = new StringBuilder();
+        for(String str: strings){
+            sb.append(str + " ");
+        }
+        System.out.println(sb.toString());
+    }
+}
